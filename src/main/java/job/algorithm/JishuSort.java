b021616e0c09565e15a66a596df85d3fdb2ed03b
Freddy <Freddy.Yufei.Zou@kingland.com>
2022-05-06 16:29:15 +0800

    commit

--- src/main/java/job/algorithm/JishuSort.java
+++ src/main/java/job/algorithm/JishuSort.java
@@ -1,35 +1,31 @@
 package job.algorithm;
 
+import java.util.stream.IntStream;
+
 public class JishuSort extends Base {
 
     public static void main(String[] args) {
-        show();
-        int max = items[0], min = items[0];
-        for (int i : items) {
-            if (i > max) {
-                max = i;
-            }
-            if (i < min) {
-                min = i;
-            }
-        }
-
-        int[] buckets = new int[max - min + 1];
-        for (int i : items) {
-            buckets[i - min]++;
-        }
-
-        for (int i = 0; i < buckets.length; i++) {
-            for (int j = 0; j < buckets[i]; j++) {
-                System.out.print(i + min);
-                System.out.print("\t");
-            }
-        }
-
+        new JishuSort().sort();
     }
 
     @Override
     void exec() {
+        final int min = IntStream.of(items).min().getAsInt();
+        final int max = IntStream.of(items).max().getAsInt();
+        int[] bucket = new int[max - min + 1];
 
+        for (int i : items) {
+            bucket[i - min]++;
+        }
+
+        int index = 0;
+        for (int i = 0; i < bucket.length; i++) {
+            if (bucket[i] == 0) {
+                continue;
+            }
+            for (int r = 0; r < bucket[i]; r++) {
+                items[index++] = i + min;
+            }
+        }
     }
 }
